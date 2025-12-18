6f844f095d496152d10f72af6804e413b9df6845
unknown <synexshahriar43@gmail.com>
2025-12-18 18:52:52 +0600

    Fix TicTacToe bugs and make board fully responsive

--- src/components/TicTacToe/TicTacToe.jsx
+++ src/components/TicTacToe/TicTacToe.jsx
@@ -1,118 +1,69 @@
-import React, { useState,useRef,reset } from 'react';
-// import React from 'react'
+import React, { useState, useRef } from 'react'
 import './TicTacToe.css'
 import circle_icon from '../assets/circle.png'
 import cross_icon from '../assets/cross.png'
 
-let data = ["","","","","","","","","",];
-
 export const TicTacToe = () => {
-    let [count,setCount] = useState(0);
-    let [lock,setLock] = useState(false);
-    let titleRef = useRef(null);
-    let box1= useRef(null);
-    let box2= useRef(null);
-    let box3= useRef(null);
-    let box4= useRef(null);
-    let box5= useRef(null);
-    let box6= useRef(null);
-    let box7= useRef(null);
-    let box8= useRef(null);
-    let box9= useRef(null);
+  const [board, setBoard] = useState(Array(9).fill(""))
+  const [isXTurn, setIsXTurn] = useState(true)
+  const [lock, setLock] = useState(false)
+  const titleRef = useRef(null)
 
-    let box_array = [box1,box2,box3,box4,box5,box6,box7,box8,box9];
+  const winPatterns = [
+    [0,1,2],[3,4,5],[6,7,8],
+    [0,3,6],[1,4,7],[2,5,8],
+    [0,4,8],[2,4,6]
+  ]
 
-    const toggle = (e, num) => {
-        if(lock) {
-            return 0;
-        }
-        if(count%2==0){
-            e.target.innerHTML = `<img src='${cross_icon}'>`;
-            data[num]="x";
-            setCount(++count);
-        }
-        else {
-            e.target.innerHTML = `<img src='${circle_icon}'>`;
-            data[num]="o";
-            setCount(++count);
-        }
+  const handleClick = (index) => {
+    if (board[index] !== "" || lock) return
 
-        checkWin();
+    const newBoard = [...board]
+    newBoard[index] = isXTurn ? "x" : "o"
+    setBoard(newBoard)
+    setIsXTurn(!isXTurn)
+    checkWin(newBoard)
+  }
 
+  const checkWin = (board) => {
+    for (let pattern of winPatterns) {
+      const [a, b, c] = pattern
+      if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+        won(board[a])
+        return
+      }
     }
+  }
 
-    const checkWin = () => {
-        if(data[0]===data[1] && data[1]===data[2] && data[2]!==""){
-            won(data[2]);
-        }
-        else if(data[3]===data[4] && data[4]===data[5] && data[5]!==""){
-            won(data[5]);
-        }
-         else if(data[6]===data[7] && data[7]===data[8] && data[8]!==""){
-            won(data[8]);
-        }
-        else if(data[0]===data[3] && data[3]===data[6] && data[6]!==""){
-            won(data[6]);
-        }
-        else if(data[1]===data[4] && data[4]===data[7] && data[7]!==""){
-            won(data[7]);
-        }
-         else if(data[2]===data[5] && data[5]===data[8] && data[8]!==""){
-            won(data[8]);
-        }
-        else if(data[0]===data[4] && data[4]===data[8] && data[8]!==""){
-            won(data[8]);
-        }
-       
-        else if(data[2]===data[4] && data[4]===data[6] && data[6]!==""){
-            won(data[6]);
-        }
-        
-    }
+   const won = (winner) => {
+    setLock(true)
+    titleRef.current.innerHTML =
+      `Congratulations!! <img src=${winner === "x" ? cross_icon : circle_icon} /> Wins`
+  }
 
-    const won = (winner) => {
-        setLock(true);
-        if(winner==="x"){
-            titleRef.current.innerHTML = `Congratulations!! <img src=${cross_icon} Wins>`;
-        }
-        else {
-            titleRef.current.innerHTML = `Congratulations!! <img src=${circle_icon} Wins>`;
-        }
-
-    }
-    const reset = () => {
-            setLock(false);
-            data = ["","","","","","","","","",];
-            titleRef.current.innerHTML = 'Tic Tac Toe Game in <span>WEB</span>';
-            box_array.map((e)=>{
-                e.current.innerHTML= "";
-            })
-        }
-
+  const resetGame = () => {
+    setBoard(Array(9).fill(""))
+    setIsXTurn(true)
+    setLock(false)
+    titleRef.current.innerHTML = 'Tic Tac Toe Game in <span>WEB</span>'
+  }
 
   return (
-    <div className='container'>
-        <h1 className='title' ref={titleRef}>Tic Tac Toe Game in <span>WEB</span></h1>
-    <div className="board">
-<div className="row1">
-    <div className="boxes" ref={box1}  onClick={(e)=>{toggle(e,0)}}></div>
-    <div className="boxes" ref={box2} onClick={(e)=>{toggle(e,1)}}></div>
-    <div className="boxes" ref={box3}onClick={(e)=>{toggle(e,2)}}></div>
-</div>
-<div className="row2">
-    <div className="boxes" ref={box4}onClick={(e)=>{toggle(e,3)}}></div>
-    <div className="boxes" ref={box5}onClick={(e)=>{toggle(e,4)}}></div>
-    <div className="boxes" ref={box6}onClick={(e)=>{toggle(e,5)}}></div>
-</div>
-<div className="row3">
-    <div className="boxes" ref={box7}onClick={(e)=>{toggle(e,6)}}></div>
-    <div className="boxes" ref={box8}onClick={(e)=>{toggle(e,7)}}></div>
-    <div className="boxes" ref={box9}onClick={(e)=>{toggle(e,8)}}></div>
-</div>
+    <div className="container">
+      <h1 className="title" ref={titleRef}>
+        Tic Tac Toe Game in <span>WEB</span>
+      </h1>
 
+      <div className="board">
+        {board.map((value, index) => (
+          <div key={index} className="boxes" onClick={() => handleClick(index)}>
+            {value === "x" && <img src={cross_icon} />}
+            {value === "o" && <img src={circle_icon} />}
+          </div>
+        ))}
+      </div>
 
-         </div>
-         <button className="reset" onClick={()=>{reset()}}>Reset</button>
+      <button className="reset" onClick={resetGame}>Reset</button>
     </div>
   )
 }
